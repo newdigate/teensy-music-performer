6bcb9f55e380e6dc987d898a7ecef9d02e98fef8
Newdigate <nicnewdigate@gmail.com>
2021-08-06 11:36:18 +0100

    add ST7735 opengl emulation

--- musicperformer.cpp
+++ musicperformer.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <MIDI.h>
+#include <ST7735_t3.h>
 
+#pragma region midi setup
 #ifdef BUILD_FOR_LINUX
     #include <RtMidiMIDI.h>
     #include <RtMidiTransport.h>
@@ -8,6 +10,20 @@
 #else
     MIDI_CREATE_DEFAULT_INSTANCE();
 #endif
+#pragma endregion
+
+#pragma region display setup
+#ifdef BUILD_FOR_LINUX
+    #include <st7735_opengl.h>
+    #include <st7735_opengl_main.h>
+    st7735_opengl tft = st7735_opengl(true, 10);
+#else
+    #define TFT_CS   6  // CS & DC can use pins 2, 6, 9, 10, 15, 20, 21, 22, 23
+    #define TFT_DC    2  //  but certain pairs must NOT be used: 2+10, 6+9, 20+23, 21+22
+    #define TFT_RST   255 // RST can use any pin
+    ST7735_t3 tft = ST7735_t3(TFT_CS, TFT_DC, TFT_RST);
+#endif
+#pragma endregion
 
 #pragma region handle midi input
 void handleNoteOn(byte channel, byte pitch, byte velocity)
@@ -24,6 +40,9 @@ void setup()
 {
     Serial.begin(9600);
 
+    tft.initR(INITR_GREENTAB);
+    tft.fillScreen(ST7735_GREEN);
+    
     // Connect the handleNoteOn function to the library,
     // so it is called upon reception of a NoteOn.
     MIDI.setHandleNoteOn(handleNoteOn);  // Put only the name of the function
@@ -40,7 +59,7 @@ void loop()
     MIDI.read();
 }
 
-#ifdef BUILD_FOR_LINUX
+#ifdef BUILD_FOR_LINUX45555
 int main() {
     setup();
     while(true) {        
